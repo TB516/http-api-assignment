fba3dfd9b926fc5d6302fe10f9e2955932069c50
TB516 <tberrios9800@gmail.com>
2024-09-22 23:35:35 -0400

    Fixed html responses crashing

--- src/responses/htmlResponses.js
+++ src/responses/htmlResponses.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../../client/style.css`);
 
-const getIndexHtml = (request, response) => {
+const getIndexHtml = (_params, request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
   response.end();
 };
 
-const getIndexCss = (request, response) => {
+const getIndexCss = (_params, request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/css' });
   response.write(css);
   response.end();
